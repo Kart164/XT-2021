5090b47f673020c108d2a6ab6a1dd56a80aa78c0
kart164 <fedor-kartashof@yandex.ru>
2021-03-18 03:16:57 +0400

    started 2-1-1, add ctors and some base metods

---
+++ task2/CharArray/StringAsCharArray.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsefullThings
+    //todo: Compare, static methods of convertation into this class, concat, override some operators, indexer, some useful methods
+{
+    public class StringAsCharArray: ICloneable
+    {
+        private char[] _chars;
+        public int Length => _chars.Length;
+
+        public StringAsCharArray():this(string.Empty)
+        {
+        }
+        public StringAsCharArray(char[] value)
+        {
+            _chars = (char[])value.Clone();
+            
+        }
+        public StringAsCharArray(string value)
+        {
+            _chars = value.ToCharArray();
+        }
+
+
+        
+
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder(_chars.Length);
+            for (int i = 0; i < _chars.Length; i++)
+            {
+                sb.Append(_chars[i]);
+            }
+            return sb.ToString();
+        }
+        public char[] ToCharArray() =>  (char[])_chars.Clone();
+
+
+
+        public override bool Equals(object obj)
+        {
+            return obj is StringAsCharArray array &&
+                   EqualityComparer<char[]>.Default.Equals(_chars, array._chars) &&
+                   Length == array.Length;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_chars, Length);
+        }
+
+
+
+
+        public object Clone()
+        {
+            var arr = new char[Length];
+            for (int i = 0; i < Length; i++)
+            {
+                arr[i] = _chars[i];
+            }
+            return arr;
+        }
+    }
+}
---
+++ task2/Task2-1-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UsefullThings;
+namespace Task2_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var str = new char[]{'a','b','c' };
+
+            var cha = new StringAsCharArray(str);
+            str[0] = '0';
+            var cha2 = new StringAsCharArray(str);
+            Console.WriteLine(cha.ToString());
+        }
+    }
+}

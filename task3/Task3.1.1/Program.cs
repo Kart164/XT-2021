0a056eb26241856437f5f420fae25cfb6d5343ec
kart164 <fedor-kartashof@yandex.ru>
2021-04-07 16:07:22 +0400

    complete task3.1.1

---
+++ task3/Task3.1.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Task3._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //in this task, counter restarts after last deleted person
+            Console.WriteLine("Введите N");
+            var n = IntInput();
+            var survivors = new Survivors(n) ;
+            Console.WriteLine("Введите, какой по счету человек будет вычеркнут каждый раунд:");
+            survivors.StartGame(CrossOutNumberInput(n));
+        }
+        static int IntInput()
+        {
+            int res;
+            while(!int.TryParse(Console.ReadLine(), out res) && res > 0)
+            {
+                Console.WriteLine("Введите значение больше 0");
+            }
+            return res;
+        }
+        static int CrossOutNumberInput(int n)
+        {
+            var res = IntInput();
+            while (res > n)
+            {
+                Console.WriteLine("Вычеркнуть человека с номером большим их колличества нельзя");
+                res = IntInput();
+            }
+            return res;
+        }
+    }
+}
---
+++ task3/Task3.1.1/SurvivorsEnumerator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3._1._1
+{
+    public class SurvivorsEnumerator : IEnumerator<int>
+    {
+        private List<int> _list;
+        private int _curIndex;
+        private int _current;
+        public int Current => _current;
+        object IEnumerator.Current => _current;
+
+        public SurvivorsEnumerator(List<int> list)
+        {
+            _list = list;
+            _curIndex = -1;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            if (++_curIndex >= _list.Count)
+            {
+                _curIndex=0;
+                _current = _list[_curIndex];
+                return true;
+            }
+            else
+            {
+                _current = _list[_curIndex];
+                return true;
+            }
+        }
+        public void GoBackOnStep()
+        {
+            if (_curIndex > 0)
+            {
+                _curIndex -= 1;
+            }
+            else _curIndex = _list.Count - 1;
+        }
+        public void Reset()
+        {
+            _curIndex = -1;
+        }
+    }
+}

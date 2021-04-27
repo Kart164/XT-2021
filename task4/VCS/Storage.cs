088642c15db18b5e2d7ee68fda0e92ae9d1ce356
kart164 <fedor-kartashof@yandex.ru>
2021-04-28 02:28:04 +0400

    add proj and class with all directories

---
+++ task4/VCS/Storage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCS
+{
+    /// <summary>
+    /// Static class to save Roots to all important directories
+    /// </summary>
+    public static class Storage
+    {
+
+        public static string DirectoryToWatch { get; private set; }
+        /// <summary>
+        /// Root to the Directory with all backups
+        /// </summary>
+        public static string BackupDirectory { get; private set; }
+        public static string Log { get; private set;  }
+
+        public static void SetDirectory(string path)
+        {
+            DirectoryToWatch = path;
+            BackupDirectory = Directory.GetParent(path) + @"\Backup";
+            Log = BackupDirectory + @"\Log.json";
+        }
+    }
+}

ed05791e093a1555fed80354d56a2648f479e9f9
kart164 <fedor-kartashof@yandex.ru>
2021-04-29 03:17:19 +0400

    add rollback methods and menu without rollback regime

--- task4/VCS/Program.cs
+++ task4/VCS/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace VCS
 {
@@ -7,13 +8,89 @@ namespace VCS
     {
         static void Main(string[] args)
         {
-            Storage.SetDirectory(@"C:\Programing\task4\watch");
-            var watcher = new Watcher();
+            Console.WriteLine($"{Environment.UserName}, specify the path to the directory " +
+                $"in the format C:\\folder\\directoryToWatch:");
+            var regex = new Regex(@"[A-Z]:[\\\w]+");
+            var path = Console.ReadLine();
+            while (!regex.IsMatch(path))
+            {
+                Console.WriteLine("WRONG FORMAT!!!! try again...");
+                path = Console.ReadLine();
+            }
+            Storage.SetDirectory(path);
             var logger = new Logger();
             logger.SaveStartStateToJson();
             logger.ReadCommits();
-            while (Console.ReadLine() != "commit") ;
-            watcher.End(logger,true);
+            var escape = false;
+            while (!escape)
+            {
+                PrintMenu();
+                int.TryParse(Console.ReadLine(), out int result);
+                switch (result)
+                {
+                    case 1:
+                        StartMonitoring(logger);
+                        break;
+                    case 2:
+                        //todo: rollback menu
+                        break;
+                    case 3:
+                        escape = true;
+                        break;
+                    default:
+                        Console.WriteLine("enter a number between 1 and 3");
+                        break;
+                }
+            }
+        }
+        static void PrintMenu()
+        {
+            Console.WriteLine("Chose mode:"+ Environment.NewLine+ "\t1. Monitoring mode"
+                +Environment.NewLine+"\t2. Rollback mode"+
+                Environment.NewLine+"\t3. Exit");
+        }
+
+        static void StartMonitoring(Logger logger)
+        {
+            Console.WriteLine("write \"commit\" to save changes, or write \"stop\" to end monitoring ");
+            var watcher = new Watcher();
+            var escape = false;
+            while (!escape)
+            {
+                var str = Console.ReadLine();
+                switch (str)
+                {
+                    case "commit":
+                        watcher.SaveCommit(logger);
+                        break;
+                    case "stop":
+                        StopMonitoring(logger, watcher);
+                        break;
+                    default:
+                        escape = true;
+                        break;
+                }
+            }
+        }
+        static void StopMonitoring(Logger logger, Watcher watcher)
+        {
+            var isStoped = false;
+            while (!isStoped)
+            {
+                Console.WriteLine("save last changes?(yes/no):");
+                var choose = Console.ReadLine();
+                switch (choose)
+                {
+                    case "yes":
+                        watcher.End(logger, true);
+                        isStoped = true;
+                        break;
+                    case "no":
+                        watcher.End(logger, false);
+                        isStoped = true;
+                        break;
+                }
+            }
         }
     }
 }
---
+++ task4/VCS/Rollback.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCS
+{
+    public static class Rollback
+    {
+        /// <summary>
+        /// this method makes a rollback of changes by rolling back the directory to the initial state
+        /// and alternately performs them from the very beginning of the list of commits to the specified index
+        /// </summary>
+        /// <param name="index">index of commit</param>
+        public static void MakeRollBack(int index,Logger logger)
+        {
+            RollBackToInitialState(logger);
+            for (int i = 0; i <= index; i++)
+            {
+                foreach (var change in logger.Commits[i].Changes)
+                {
+                    switch (change.ChangeType)
+                    {
+                        case WatcherChangeTypes.Created:
+                            File.Create(change.FilePath);
+                            break;
+                        case WatcherChangeTypes.Changed:
+                            File.Delete(change.FilePath);
+                            File.WriteAllText(change.FilePath, change.FileContent);
+                            break;
+                        case WatcherChangeTypes.Deleted:
+                            File.Delete(change.FilePath);
+                            break;
+                        case WatcherChangeTypes.Renamed:
+                            File.Copy(change.OldFullPath, change.FilePath);
+                            File.Delete(change.OldFullPath);
+                            break;
+                    }
+                }
+            }
+        }
+        public static void RollBackToInitialState(Logger logger)
+        {
+            var files = Directory.GetFiles(Storage.DirectoryToWatch);
+            foreach (var file in files)
+            {
+                File.Delete(file);
+            }
+            Directory.Delete(Storage.DirectoryToWatch, true);
+            Directory.CreateDirectory(Storage.DirectoryToWatch);
+            foreach (var file in logger.InitialFileList)
+            {
+                if (!Directory.Exists(Path.GetDirectoryName(file.FilePath)))
+                    Directory.CreateDirectory(Path.GetDirectoryName(file.FilePath));
+                File.WriteAllText(file.FilePath, file.Content);
+            }
+        }
+        
+    }
+}
